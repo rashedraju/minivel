55aa25802f1f4e2773fccdea9df3b8e66da6847e
rashedraju <rashedraju.cs@gmail.com>
2021-09-26 00:45:12 +0600

    Update register.php

--- views/register.php
+++ views/register.php
@@ -1,8 +1,19 @@
+<?php
+/**
+ * @var \App\core\View $this
+ * @var \App\core\UserModel $model
+ */
+
+use App\core\form\Form;
+
+$this->title = "Register";
+?>
 <h1>Create an account</h1>
-<?php $form = \App\core\form\Form::begin("/register", "post")?>
-<?php echo $form->field($model, "username") ?>
-<?php echo $form->field($model, "email") ?>
-<?php echo $form->field($model, "password")->passwordField() ?>
-<?php echo $form->field($model, "confirmPassword")->passwordField() ?>
+<?php $form = Form::begin("/register", "post")?>
+<?php echo $form->inputField($model, "username") ?>
+<?php echo $form->inputField($model, "email") ?>
+<?php echo $form->inputField($model, "password")->passwordField() ?>
+<?php echo $form->inputField($model, "confirmPassword")->passwordField() ?>
 <button type="submit" class="btn btn-primary">Register</button>
 <?php $form->end() ?>
+
