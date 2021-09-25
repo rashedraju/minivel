6573b3e6408896d377795c344690817589859887
rashedraju <rashedraju.cs@gmail.com>
2021-09-26 00:38:48 +0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->safeLoad();
 
 $config = [
+    "userClass" => \App\models\User::class,
     "db" => [
         "dsn" => $_ENV['DB_DSN'],
         "username" => $_ENV['DB_USER'],
@@ -20,10 +21,12 @@ $app = new Application(dirname(__DIR__), $config);
 
 $app->router->get("/", [SiteController::class, "home"]);
 $app->router->get("/contact", [SiteController::class, "contact"]);
-$app->router->post("/contact", [SiteController::class, "contactHandler"]);
+$app->router->post("/contact", [SiteController::class, "contact"]);
 $app->router->get("/login", [AuthController::class, "login"]);
 $app->router->post("/login", [AuthController::class, "login"]);
 $app->router->get("/register", [AuthController::class, "register"]);
 $app->router->post("/register", [AuthController::class, "register"]);
+$app->router->get("/logout", [AuthController::class, "logout"]);
+$app->router->get("/profile", [AuthController::class, "profile"]);
 
 $app->run();
--- views/home.php
+++ views/home.php
@@ -1,2 +1,10 @@
+<?php
+/**
+ * @var \App\core\View $this
+ * @var $name
+ */
+$this->title = "Home";
+?>
+
 <h1>Home</h1>
 <h1>Welcome <?php echo $name; ?></h1>
