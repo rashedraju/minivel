1212d758dbff6b9bf9fc3162000a5caef56068b5
rashedraju <rashedraju.cs@gmail.com>
2021-09-26 00:44:24 +0600

    Update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,22 +4,28 @@ namespace App\controllers;
 
 use App\core\Application;
 use App\core\Request;
+use App\core\Response;
+use App\models\ContactForm;
 
 class SiteController extends Controller
 {
     public function home(){
-        $users = [
-            "name" => "John Doe"
-        ];
-
-        return $this->render("home", $users);
+        $displayName = Application::isGuest() ? "Guest" : Application::$app->user->getDisplayName();
+        return $this->render("home", ["name" => $displayName]);
     }
-    public function contact(){
-        return $this->render("contact");
-    }
+    public function contact(Request $request, Response $response){
+        $contactForm = new ContactForm();
 
-    public function contactHandler(Request $request): string{
-        print_r($request->getBody());
-        return "Handling contact...";
+        if($request->isPost()){
+            $contactForm->loadData($request->getBody());
+            if($contactForm->validate() && $contactForm->send()){
+                Application::$app->session->setFlashMessage("success", "Thanks for contacting with us.");
+                $response->redirect("/contact");
+            }
+        }
+        // render login view
+        return $this->render("contact", [
+            "model" => $contactForm
+        ]);
     }
 }
