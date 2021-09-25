fd3318606a9ed8a299da2870c9cda98047f344ad
rashedraju <rashedraju.cs@gmail.com>
2021-09-26 00:39:06 +0600

    Update login view

--- views/login.php
+++ views/login.php
@@ -1,12 +1,17 @@
+<?php
+/**
+ * @var \App\core\View $this
+ * @var \App\models\User $model
+ */
+
+use App\core\form\Form;
+
+$this->title = "LoginForm";
+?>
+
 <h1>Login</h1>
-<form action="/login" method="post">
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" name="password" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Login</button>
-</form>
+<?php $form = Form::begin("/login", "post")?>
+<?php echo $form->inputField($model, "email") ?>
+<?php echo $form->inputField($model, "password")->passwordField() ?>
+<button type="submit" class="btn btn-primary">Login</button>
+<?php $form->end() ?>
