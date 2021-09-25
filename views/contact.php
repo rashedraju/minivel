278e108c6935d4fe95c26e46ffbf564a6914f54c
rashedraju <rashedraju.cs@gmail.com>
2021-09-26 00:33:58 +0600

    Update contact view

--- views/contact.php
+++ views/contact.php
@@ -1,16 +1,16 @@
+<?php
+/**
+ * @var \App\core\View $this
+ * @var \App\models\ContactForm $model
+ */
+$this->title = "Contact";
+use App\core\form\Form;
+
+?>
 <h1>Contact</h1>
-<form action="" method="post">
-    <div class="mb-3">
-        <label for="subject" class="form-label">Subject</label>
-        <input type="text" name="subject" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" name="email" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label" for="textarea">Body</label>
-        <textarea name="textarea" class="form-control"></textarea>
-    </div>
+<?php $form = Form::begin("/contact", "post")?>
+<?php echo $form->inputField($model, "name") ?>
+<?php echo $form->inputField($model, "email") ?>
+<?php echo $form->textareaField($model, "body") ?>
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php $form->end() ?>
